49c85e7959c465c220ac173eceb1236b59a8c80e
Alex Ojeda Copa <alex-roc@users.noreply.github.com>
2022-05-20 19:37:33 -0400

    fix: import logo

--- ex-0/react-ex/src/components/header/Logo.js
+++ ex-0/react-ex/src/components/header/Logo.js
@@ -1,10 +1,11 @@
+import logoImage from './../../assets/logo-khan.png'
 const style = {
     height: '3rem'
 }
 export default function Logo(){
     return (
         <img style={style}
-            src={require('../../assets/logo-khan.png')}
+            src={logoImage}
             className="logo" alt="This is the Khan Academy logo."/>
 
     );
