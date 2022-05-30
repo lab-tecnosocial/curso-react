06dafa1302eedbbe7d45611859a2e8718adbeb5f
erickwgc <erickxs78@gmail.com>
2022-05-30 16:38:28 -0400

    Ejercicio useState y useEffect

---
+++ ex-3-usestate-useeffect/src/App.js
@@ -0,0 +1,43 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import Personaje from "./Personaje";
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [personajes, setPersonajes] = useState([]);
+
+  useEffect(() => {
+    getDataFromAPI(); //Traer datos desde una BD o API
+  }, []);
+
+  const getDataFromAPI = async () => {
+    try {
+      const response = await fetch(
+        "https://naruto-api.herokuapp.com/api/v1/characters"
+      );
+      await response.json().then((data) => {
+        setPersonajes(data);
+        setLoading(false);
+        console.log(data);
+      });
+    } catch (error) {
+      console.log("Hubo un problema: " + error.message);
+    }
+  };
+
+  return (
+    <div>
+      Ejercicio useState y useEffect
+      <hr />
+      {loading ? (
+        <h1>Cargando...</h1>
+      ) : (
+        personajes.map((item) => (
+          <Personaje key={item.id} name={item.name} image={item.images[0]} />
+        ))
+      )}
+    </div>
+  );
+}
+
+export default App;
